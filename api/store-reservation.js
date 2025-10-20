559f61be6c4b10c21e2e5ab8efd7efa4f0bf8b86
BIKESHOPROMEO <tama@realeyes.co.jp>
2025-10-20 18:34:44 +0900

    Add files via upload

--- api/store-reservation.js
+++ api/store-reservation.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
         return res.status(405).json({ message: "Method Not Allowed" });
     }
 
-    const GAS_URL = "https://script.google.com/macros/s/AKfycbyLiXkqrjliK3z4K8ZKun4YXWc7z3nN3iZt_oNH1q115YJ3ZzaUuUzaCuxUZDgjZHuU9A/exec";
+    const GAS_URL = "https://script.google.com/macros/s/AKfycbw0xAlekbbME08YaiMQMMB5ngfCDaMEWXicbyU1VlFl8quo1srYmibMD6BPWjdzj_Tb7g/exec";
     const data = req.body;
 
     try {
